3b4b2a43245c7e5370a52bd17a3b34d18930fbc9
Bruno Girome <brunogirome@gmail.com>
2021-05-16 21:52:11 -0300

    Implemented mirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createServer } from "miragejs";
 import { App } from "./App";
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'New bookcase',
+          amount: 125.99,
+          category: 'Furniture',
+          date: new Date(),
+        },
+        {
+          id: 2,
+          title: 'Engery Taxes',
+          amount: 101.54,
+          category: 'Taxes',
+          date: new Date(),
+        },
+        {
+          id: 3,
+          title: 'BSA3 earnings',
+          amount: 654.47,
+          category: 'Stock earnings',
+          date: new Date(),
+        }
+      ];
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
