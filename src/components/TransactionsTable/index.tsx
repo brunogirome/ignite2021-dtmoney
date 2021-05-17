30cc9eea93e6331291a0da4de98faf2aa6f95082
Bruno Girome <brunogirome@gmail.com>
2021-05-16 21:57:37 -0300

    Implemented axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
   useEffect(() => {
-    fetch("http://localhost:3000/api/transactions")
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+    api.get("transactions")
+      .then((response) => console.log(response.data));
   }, []);
 
   return (
@@ -13,7 +13,7 @@ export function TransactionsTable() {
       <table>
         <thead>
           <tr>
-            <th>Tittle</th>
+            <th>Title</th>
             <th>Value</th>
             <th>Category</th>
             <th>Date</th>
