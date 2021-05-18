22c66e82fc9f0ce74caa9e54a155f2b45b284279
Bruno Girome <brunogirome@gmail.com>
2021-05-17 21:41:35 -0300

    Basic modal features implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
+import Modal from "react-modal";
+
 import Dashboard from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTranscationModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTranscationModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header handleOpenNewTranscationModal={handleOpenNewTranscationModal} />
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTranscationModal}
+      >
+        <h2>Register new transaction</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import logoImg from "../../assets/logo.svg";
+
 import { Container, Content } from "./styles";
 
-export function Header() {
+interface HeaderProps {
+  handleOpenNewTranscationModal: () => void;
+}
+
+export function Header({ handleOpenNewTranscationModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">New transaction</button>
+        <button type="button" onClick={handleOpenNewTranscationModal}>
+          New transaction
+        </button>
       </Content>
     </Container>
   );
